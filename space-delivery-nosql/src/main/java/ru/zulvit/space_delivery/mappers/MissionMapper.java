1f4cdbad4779567821993716ae5cdfccebb7acb8
savva <zulvit.sk@gmail.com>
2023-11-09 01:32:30 +0300

    Bugfix

--- space-delivery-nosql/src/main/java/ru/zulvit/space_delivery/mappers/MissionMapper.java
+++ space-delivery-nosql/src/main/java/ru/zulvit/space_delivery/mappers/MissionMapper.java
@@ -1,36 +1,21 @@
 package ru.zulvit.space_delivery.mappers;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.zulvit.space_delivery.dto.request.MissionRequestDto;
 import ru.zulvit.space_delivery.dto.response.MissionResponseDto;
 import ru.zulvit.space_delivery.model.Mission;
-import ru.zulvit.space_delivery.service.CargoService;
-import ru.zulvit.space_delivery.service.RocketService;
 
 @Component
 public class MissionMapper {
     private final ModelMapper modelMapper;
-    private final RocketService rocketService;
-    private final CargoService cargoService;
 
-    @Autowired
-    public MissionMapper(ModelMapper modelMapper, RocketService rocketService, CargoService cargoService) {
+    public MissionMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
-        this.rocketService = rocketService;
-        this.cargoService = cargoService;
 
-        modelMapper.addMappings(new PropertyMap<MissionRequestDto, Mission>() {
-            protected void configure() {
-                skip(destination.getId());
-            }
+        modelMapper.typeMap(MissionRequestDto.class, Mission.class).addMappings(mapper -> {
+            mapper.skip(Mission::setId);
         });
-    }
-
-    public MissionRequestDto convertToRequestDto(Mission mission) {
-        return modelMapper.map(mission, MissionRequestDto.class);
     }
 
     public MissionResponseDto convertToResponseDto(Mission mission) {
@@ -38,14 +23,6 @@ public class MissionMapper {
     }
 
     public Mission convertToEntityFromRequestDto(MissionRequestDto missionRequestDto) {
-        Mission mission = modelMapper.map(missionRequestDto, Mission.class);
-        mission.setRocketId(rocketService.getRocketById(missionRequestDto.getRocketId()).getId());
-        mission.setCargoId(cargoService.getCargoById(missionRequestDto.getCargoId()).getId());
-
-        return mission;
-    }
-
-    public Mission convertToEntityFromResponseDto(MissionResponseDto missionResponseDto) {
-        return modelMapper.map(missionResponseDto, Mission.class);
+        return modelMapper.map(missionRequestDto, Mission.class);
     }
 }
